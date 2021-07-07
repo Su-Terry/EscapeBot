4ebcaf9bb1b357f299e7bd0b852b851b7a4b0977
oceansfavor <51773435+Su-Terry@users.noreply.github.com>
2021-07-07 12:18:02 +0800

    v3.1

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 from discord.ext import commands
 import json, os
 
-with open("setting.json", 'r', encoding="utf8") as jfile:
-    jdata = json.load(jfile)
 
 bot = commands.Bot(command_prefix='%')
 
@@ -15,13 +13,11 @@ async def reload(ctx, extension):
     bot.reload_extension(f"cmds.{extension}")
     await ctx.send(f"Re - Loaded {extension} done.")
 
-@bot.command()
-async def get_link(ctx):
-    await ctx.channel.send(jdata['invite_link'])
-
 for Filename in os.listdir(r'./cmds'):
     if Filename.endswith('.py'):
         bot.load_extension(f"cmds.{Filename[:-3]}")
 
 if __name__ == '__main__':
+    with open("setting.json", 'r', encoding="utf8") as jfile:
+        jdata = json.load(jfile)
     bot.run(os.environ['TOKEN'])
