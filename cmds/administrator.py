2d71e7b73217e0a8b5a3c0d3cd234572e2cd6260
oceansfavor <51773435+Su-Terry@users.noreply.github.com>
2021-07-08 13:11:34 +0800

    v3.3
    "v3.2": 24小時運作，修復bug
    "v3.3": 新增訊息清理功能(clear)，伺服器管理員可清除該頻道訊息

--- cmds/administrator.py
+++ cmds/administrator.py
@@ -1,3 +1,4 @@
+from discord.ext.commands.core import has_permissions
 from core.classes import Cog_Extension
 from discord.ext import commands
 import json
@@ -9,7 +10,8 @@ class Administrator(Cog_Extension):
     def __init__(self, bot):
         super().__init__(bot)
     
-    @commands.command()
+    @has_permissions(administator=True)
+    @commands.command(brief='刪除{num}則訊息，"%clear {num}"')
     async def clear(self, ctx, num:int):
         await ctx.message.delete()
         await ctx.channel.purge(limit=num)
