0539d99ae317bac691d8700ca59a04d048084b78
oceansfavor <51773435+Su-Terry@users.noreply.github.com>
2021-07-09 22:19:51 +0800

    v3.5.6

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 
 
-bot = commands.Bot(command_prefix='', help_command='<default-help-command>')
+bot = commands.Bot(command_prefix='', help_command=commands.MinimalHelpCommand())
 
 @bot.event
 async def on_ready():
