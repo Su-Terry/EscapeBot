e218274444a5b26350e4b5070d6046b873864f6d
oceansfavor <51773435+Su-Terry@users.noreply.github.com>
2021-07-09 23:41:01 +0800

    v3.6.3

--- bot.py
+++ bot.py
@@ -26,11 +26,11 @@ async def help(ctx):
     with open('setting.json', 'r', encoding='utf8') as jfile:
         jdata = json.load(jfile)
     embed = discord.Embed(title='Help for EscapeBot',
-            description="逃脫遊戲: 版本{}".format(jdata['version']))
+        description="文字版逃脫遊戲: 版本{}".format(jdata['version']))
     
-    embed.add_field(name='Escape', value='escape -> 進入遊戲', inline=False)
-    embed.add_field(name='General', value='邀請連結\n版本紀錄', inline=False)
-    embed.add_field(name='Administrator', value='clear {num} -> 清理訊息', inline=False)
+    embed.add_field(name='Escape', value='`escape` -> 進入遊戲', inline=False)
+    embed.add_field(name='General', value='`邀請連結`  `版本紀錄`', inline=False)
+    embed.add_field(name='Administrator', value='`clear {num}` -> 清理{num}則訊息', inline=False)
     
     if os.path.isfile(path):
         with open(path, 'r', encoding='utf8') as jfile:
@@ -38,7 +38,7 @@ async def help(ctx):
         if jdata['inGame'] == 'TRUE':
             cmds = ''
             for cmd in jdata['cmds']:
-                cmds += f'{cmd}\n'
+                cmds += f'`{cmd}`  '
             if cmds != '':
                 embed.add_field(name='遊戲中指令', value=cmds, inline=False)
     await ctx.send(embed=embed)
